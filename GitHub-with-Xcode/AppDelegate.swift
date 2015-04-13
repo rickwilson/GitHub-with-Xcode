a0ca2a7a4d9862766f22bbda55b8e092a567f532
Rick Wilson <rick@teamwilson.org>
2015-04-13 14:36:50 -0600

    test2

--- GitHub-with-Xcode/AppDelegate.swift
+++ GitHub-with-Xcode/AppDelegate.swift
@@ -13,7 +13,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
     
-    //test
+    //test2
 
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
